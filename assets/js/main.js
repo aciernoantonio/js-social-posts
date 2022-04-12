9fa48aa591504f7622a1ce13171fce1d59920e71
aciernoantonio <aciernoantonio.int@gmail.com>
2022-04-12 15:51:47 +0200

    initial commit and objects declaration

---
+++ assets/js/main.js
@@ -0,0 +1,22 @@
+//crea array di oggetti che rappresenta ciascun post
+let posts = [
+    {
+        id: 1, 
+        user_name: "Bronto Paolo",
+        user_photo: "http://assets.stickpng.com/images/585e4bf3cb11b227491c339a.png",
+        date: "07-05-2022",
+        text_post: "Whatever is good for your soul, do that",
+        post_photo: "https://i.picsum.photos/id/232/300/300.jpg?hmac=CRMHCkif6NslMZy3Qw-PaoymQMf9L_t92kK3xa_Zr7g",
+        likes: 39
+    },
+
+    {
+        id: 2, 
+        user_name: "Cannon Brando",
+        user_photo: "http://assets.stickpng.com/images/585e4bf3cb11b227491c339a.png",
+        date: "07-03-2022",
+        text_post: "Whatever is good for your soul, don't do that",
+        post_photo: "https://i.picsum.photos/id/294/300/300.jpg?hmac=F7rT8nOUlyZecLe_lkllejN6bqMCuLQDt9iVfKSeyQg",
+        likes: 39
+    }
+]
